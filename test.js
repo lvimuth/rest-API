36c380a804be82afce375bc64245a0733b58fdfd
Lakshitha Vimuth <lvimuthfb@gmail.com>
2024-08-22 22:23:08 +0400

    HTML form

--- test.js
+++ test.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const port = 5500;
 const host = "127.0.0.1";
+const path = require("path");
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Middleware to parse URL-encoded bodies (from HTML forms)
+app.use(express.urlencoded({ extended: true }));
 
 const books = [
   { title: "Book 1", author: "Author 1", publicationYear: 2000 },
@@ -9,20 +16,29 @@ const books = [
   { title: "Book 3", author: "Author 3", publicationYear: 2010 },
 ];
 
+// Serve the HTML file
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "test.html"));
+});
+
+// GET all books
 app.get("/books", (req, res) => {
   res.json(books);
 });
 
-app.post("/books", (req, res) => {
+// POST to add a new book
+app.post("/books/add", (req, res) => {
   const newBook = {
     id: books.length + 1,
     title: req.body.title,
     author: req.body.author,
+    publicationYear: parseInt(req.body.publicationYear),
   };
   books.push(newBook);
   res.status(201).json(newBook);
 });
 
+// Start the server
 app.listen(port, host, () => {
   console.log(`Server is running on http://${host}:${port}`);
 });
